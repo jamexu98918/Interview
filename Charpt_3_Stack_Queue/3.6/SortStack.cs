919928604cec6b3d7d9c0b119df2639b3c2b3870
jamexu98918 <jamesxu@outlook.com>
2013-09-27 17:01:16 -0700

    stack

---
+++ Charpt_3_Stack_Queue/3.6/SortStack.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Write a program to sort a stack in ascending order(with biggest items on the top).
+ * you may use at most one additional stack to hold items, but you may not copy the elements
+ * into any other data structure(such as an array). the stack supports the following operations:
+ * push, pop, peek, and isEmpty(or count);
+ */ 
+namespace SortStack
+{
+    class SortStack
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(7);
+            stack.Push(10);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(6);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(9);
+            stack.Push(10);
+            Stack<int> temp = new Stack<int>();
+
+            Sort1<int>(stack);
+
+            //Sort2<int>(stack,temp,1);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("{0}", stack.Pop());
+            }
+            Console.WriteLine("End");
+            Console.Read();
+
+        }
+
+
+        public static void Sort1<T>(Stack<T> s) where T : IComparable
+        {
+            Stack<T> temp = new Stack<T>();
+            while (s.Count > 0)
+            {
+                T sTop = s.Pop();
+                while(temp.Count>0 && temp.Peek().CompareTo(sTop)<0)
+                {
+                    s.Push(temp.Pop());
+                }
+
+                temp.Push(sTop);
+                
+            }
+
+            while (temp.Count > 0)
+            {
+                s.Push(temp.Pop());
+            }
+
+        }
+
+
+        //sort stack s by temp stack space with specify order, order = 1: ascending, order = -1 : Desending
+        public static void Sort2<T>(Stack<T> s, Stack<T> temp,int order) where T : IComparable
+        {
+            bool isSwitch = false;
+
+            while (s.Count > 0)
+            {
+                T item = s.Pop();
+                if (temp.Count>0)
+                {
+                    T temptop = temp.Peek();
+                    if (temptop.CompareTo(item) * order < 0)
+                    {
+                        temp.Pop();
+                        temp.Push(item);
+                        temp.Push(temptop);
+                        isSwitch = true;
+                    }
+                    else
+                    {
+                        temp.Push(item);
+                    }
+                }
+                else
+                {
+                    temp.Push(item);
+                }
+                
+            }
+
+            if (isSwitch)
+            {
+                Sort2<T>(temp, s, order * -1);
+            }
+            else
+            {
+                if (order == 1)
+                {
+                    while (temp.Count > 0)
+                    {
+                        s.Push(temp.Pop());
+                    }
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Charpt_3_Stack_Queue/3.7/AnimalShelter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+ * An animal shelter holds only dogs and cats, and operates on the strictly "first in,
+ * first out" basis. People must adopt either the "oldest" (based on arrival time) of 
+ * all animals at the shelter, or they can select  whether they would perfer a dog or
+ * a cat(and will receive the oldest animal of that type). they cannot select which
+ * specific animal they would like. Create the data structures to maintain this system
+ * and implement operations such as enqueue, dequeueAny, dequeueDog and dequeueCat.
+ * you may use the build-in LinkedList data structure
+ * 
+ */ 
+namespace AnimalShelter
+{
+    public class AnimalShelter
+    {
+        private LinkedList<object> animalQueue = new LinkedList<object>();
+
+        public void enqueue(object o)
+        {
+            animalQueue.AddFirst(o);
+        }
+
+        public object dequeueAny()
+        {
+            object o = animalQueue.Last.Value;
+            animalQueue.RemoveLast();
+            return o;
+        }
+
+        public dog dequeueDog()
+        {
+            LinkedListNode<object> node=animalQueue.First;
+            LinkedListNode<object> lastDog = null;
+            while (node != null)
+            {
+                if (node.Value is dog)
+                {
+                    lastDog = node;
+                }
+                node = node.Next;
+            }
+            animalQueue.Remove(lastDog);
+            return (dog)lastDog.Value;
+                
+        }
+
+        public cat dequeueCat()
+        {
+            LinkedListNode<object> node = animalQueue.First;
+            LinkedListNode<object> lastCat = null;
+            while (node != null)
+            {
+                if (node.Value is cat)
+                {
+                    lastCat = node;
+                }
+                node = node.Next;
+            }
+            animalQueue.Remove(lastCat);
+            return (cat)lastCat.Value;
+
+        }
+    }
+
+
+    public class cat
+    {
+        public string name;
+        public cat(string _name)
+        {
+            this.name = _name;
+        }
+    }
+
+    public class dog
+    {
+        public string name;
+        public dog(string _name)
+        {
+            this.name = _name;
+        }
+    }
+}
